ea8a625d57f72be3624ff28df79c5a57dffa765d
jon brookes <marshyon@gmail.com>
2022-06-06 17:05:07 +0100

    fix up ansible version

--- packages.sh
+++ packages.sh
@@ -7,13 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 #
 apt-get update 
 apt upgrade -y
-apt install git vim zsh neofetch rsync -y 
-
-# wget https://releases.hashicorp.com/terraform/1.2.2/terraform_1.2.2_linux_amd64.zip 
-# unzip terraform*.zip 
-# chmod +x terraform 
-# mv terraform /usr/local/bin/terraform
-# rm terraform*.zip
+apt install git vim zsh neofetch rsync python-pip python-dev -y 
 
 #
 # AWS CLI
@@ -55,37 +49,6 @@ ln -s /usr/local/bin/kubectl_1.24.0 /usr/local/bin/kubectl
 #
 # ANSIBLE
 #
-# PIPINST="get-pip.py"
 
-# if [[ -f $PIPINST ]]; then
-#   echo "$PIPINST exists"
-# else
-#   echo "$PIPINST does not exist"
-#   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-#   python3 get-pip.py --user
-# fi
-
-# PATHSTR='export PATH=$PATH:'${HOME}'/.local/bin'
-
-# case `grep -Fx "$PATHSTR" "${HOME}/.bashrc" >/dev/null; echo $?` in
-#   0)
-#     # code if found
-#     echo "path found"
-#     ;;
-#   1)
-#     # code if not found
-#     echo "not there"
-#     echo "export PATH=\$PATH:${HOME}/.local/bin" >> ~/.bashrc
-#     ;;
-#   *)
-#     # code if an error occurred
-#     echo "error trying to find path string in .bashrc"
-#     exit 1
-#     ;;
-# esac
-
-# source ~/.bashrc
-# pip install virtualenv
-
-# python3 -m virtualenv ansible
-# source ansible/bin/activate
+# pip install ansible==2.3.3.0
+# pip install ansible==2.9
